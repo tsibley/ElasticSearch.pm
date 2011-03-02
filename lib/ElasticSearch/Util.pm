8f06f0a61ea60baf243a2601f4faf0356da1749f
Clinton Gormley <clinton@iannounce.co.uk>
2011-03-02 14:01:13 +0100

    Added filter_keywords() back in to ElasticSearch::Util, in case somebody is using it

--- lib/ElasticSearch/Util.pm
+++ lib/ElasticSearch/Util.pm
@@ -6,7 +6,39 @@ use ElasticSearch::Error();
 
 require Exporter;
 our @ISA       = qw(Exporter);
-our @EXPORT_OK = qw(parse_params throw build_error);
+our @EXPORT_OK = qw(filter_keywords parse_params throw build_error);
+
+#===================================
+sub filter_keywords {
+#===================================
+    local $_ = shift;
+
+    s{[^[:alpha:][:digit:] \-+'"*@\._]+}{ }g;
+
+    return '' unless /[[:alpha:][:digit:]]/;
+
+    s/\s*\b(?:and|or|not)\b\s*/ /gi;
+
+    # remove '-' that don't have spaces before them
+    s/(?<! )-/\ /g;
+
+    # remove the spaces after a + or -
+    s/([+-])\s+/$1/g;
+
+    # remove + or - not followed by a letter, number or "
+    s/[+-](?![[:alpha:][:digit:]"])/ /g;
+
+    # remove * without 3 char prefix
+    s/(?<![[:alpha:][:digit:]\-@\._]{3})\*/ /g;
+
+    my $quotes = (tr/"//);
+    if ( $quotes % 2 ) { $_ .= '"' }
+
+    s/^\s+//;
+    s/\s+$//;
+
+    return $_;
+}
 
 #===================================
 sub parse_params {
@@ -88,6 +120,56 @@ ElasticSearch::Util - Util subs for ElasticSearch
 
 ElasticSearch::Util provides various subs useful to other modules in
 ElasticSearch.
+
+The only sub useful to users is L</"filter_keywords()">, which can be
+exported.
+
+=head1 SYNOPSIS
+
+    use ElasticSearch::Util qw(filter_keywords);
+
+    my $filtered = filter_keywords($unfiltered)
+
+=head1 SUBROUTINES
+
+=head2 C<filter_keywords()>
+
+This tidies up a string to be used as a query string in (eg)
+L<ElasticSearch/"search()"> so that user input won't cause a search query
+to return an error.
+
+It is not flexible at all, and may or may not be useful to you.
+
+Have a look at L<ElasticSearch::QueryParser> which gives you much more control
+over your query strings.
+
+The current implementation does the following:
+
+=over
+
+=item * Removes any character which isn't a letter, a number, a space or
+  C<-+'"*@._>.
+
+=item * Removes C<and>, C<or> and C<not>
+
+=item * Removes any C<-> that doesn't have a space in front of it ( "foo -bar")
+      is acceptable as it means C<'foo' but not with 'bar'>
+
+=item * Removes any space after a C<+> or C<->
+
+=item * Removes any C<+> or C<-> which is not followed by a letter, number
+      or a double quote
+
+=item * Removes any C<*> that doesn't have at least 3 letters before it, ie
+      we only allow wildcard searches on words with at least 3 characters
+
+=item * Closes any open double quotes
+
+=item * Removes leading and trailing whitespace
+
+=back
+
+YMMV
 
 =head1 LICENSE AND COPYRIGHT
 
