50bdf003351156e2e27c549dc4220f3fc7f0744f
Clinton Gormley <clinton@iannounce.co.uk>
2011-03-20 15:23:53 +0100

    Released version 0.34

--- lib/ElasticSearch.pm
+++ lib/ElasticSearch.pm
@@ -7,7 +7,7 @@ use ElasticSearch::Error();
 use ElasticSearch::RequestParser;
 use ElasticSearch::Util qw(throw parse_params);
 
-our $VERSION = '0.33';
+our $VERSION = '0.34';
 our $DEBUG   = 0;
 
 #===================================
