c53a288262d850ab9047946a1b77303c8dfbca88
Clinton Gormley <clinton@iannounce.co.uk>
2011-04-24 21:00:19 +0200

    Fixed the test for index_status(recovery=>1)

--- t/request_tests/index_status.pl
+++ t/request_tests/index_status.pl
@@ -21,7 +21,6 @@ qr/ElasticSearch::Error::Missing/, ' - index missing';
 ok $r= $es->index_status( index => 'es_test_1', recovery => 1, snapshot => 1 )
     ->{indices}{es_test_1}{shards}{0}, ' - recovery and snapshot';
 
-ok $r->[0]{peer_recovery},    ' - peer recovery';
-ok $r->[1]{gateway_recovery}, ' - gateway recovery';
+ok $r->[0]{peer_recovery}||$r->[0]{gateway_recovery},    ' - recovery';
 
 1;
