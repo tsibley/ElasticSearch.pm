83fbce4ec5462994637e9b18d5c622c09618aeec
Clinton Gormley <clinton@iannounce.co.uk>
2011-05-01 13:52:10 +0200

    Updated t/request_tests/open_close_index.pl to wait for longer - reopening an index can still take more time than it should

--- t/request_tests/open_close_index.pl
+++ t/request_tests/open_close_index.pl
@@ -14,7 +14,7 @@ throws_ok sub { $es->count( index => 'es_test_1', match_all => {} ) },
 
 ok $es->open_index( index => 'es_test_1' )->{ok}, 'Opened index';
 
-wait_for_es(2);
+wait_for_es(3);
 
 is $es->count( index => 'es_test_1', match_all => {} )->{count}, 1,
     ' - index reopened';
