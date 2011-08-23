01e038a422a945c2afa094004b45bb843086d632
Clinton Gormley <clinton@iannounce.co.uk>
2011-08-23 18:10:46 +0200

    Moved the HTTP status codes from the Thrift backend into ElasticSearch::Transport, to make them available to other backends

--- lib/ElasticSearch/Transport.pm
+++ lib/ElasticSearch/Transport.pm
@@ -426,6 +426,60 @@ sub clear_clients {
 sub JSON { shift()->{_JSON} }
 #===================================
 
+my %Statuses = (
+    100 => 'CONT',
+    101 => 'SWITCHING_PROTOCOLS',
+    200 => 'OK',
+    201 => 'CREATED',
+    202 => 'ACCEPTED',
+    203 => 'NON_AUTHORITATIVE_INFORMATION',
+    204 => 'NO_CONTENT',
+    205 => 'RESET_CONTENT',
+    206 => 'PARTIAL_CONTENT',
+    207 => 'MULTI_STATUS',
+    300 => 'MULTIPLE_CHOICES',
+    301 => 'MOVED_PERMANENTLY',
+    302 => 'FOUND',
+    303 => 'SEE_OTHER',
+    304 => 'NOT_MODIFIED',
+    305 => 'USE_PROXY',
+    307 => 'TEMPORARY_REDIRECT',
+    400 => 'BAD_REQUEST',
+    401 => 'UNAUTHORIZED',
+    402 => 'PAYMENT_REQUIRED',
+    403 => 'FORBIDDEN',
+    404 => 'NOT_FOUND',
+    405 => 'METHOD_NOT_ALLOWED',
+    406 => 'NOT_ACCEPTABLE',
+    407 => 'PROXY_AUTHENTICATION',
+    408 => 'REQUEST_TIMEOUT',
+    409 => 'CONFLICT',
+    410 => 'GONE',
+    411 => 'LENGTH_REQUIRED',
+    412 => 'PRECONDITION_FAILED',
+    413 => 'REQUEST_ENTITY_TOO_LARGE',
+    414 => 'REQUEST_URI_TOO_LONG',
+    415 => 'UNSUPPORTED_MEDIA_TYPE',
+    416 => 'REQUESTED_RANGE_NOT_SATISFIED',
+    417 => 'EXPECTATION_FAILED',
+    422 => 'UNPROCESSABLE_ENTITY',
+    423 => 'LOCKED',
+    424 => 'FAILED_DEPENDENCY',
+    500 => 'INTERNAL_SERVER_ERROR',
+    501 => 'NOT_IMPLEMENTED',
+    502 => 'BAD_GATEWAY',
+    503 => 'SERVICE_UNAVAILABLE',
+    504 => 'GATEWAY_TIMEOUT',
+    506 => 'INSUFFICIENT_STORAGE',
+);
+
+#===================================
+sub http_status {
+#===================================
+    my $code = $_[1] || 0;
+    return $Statuses{$code} || 'Unknown code ' . $code;
+}
+
 #===================================
 sub register {
 #===================================
