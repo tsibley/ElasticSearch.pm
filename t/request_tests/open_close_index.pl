432cc4dce8ab00e71a798c5e267fb4524c023b3b
Clinton Gormley <clinton@iannounce.co.uk>
2012-02-17 19:51:05 +0100

    Wait after closing the index - sometimes times out

--- t/request_tests/open_close_index.pl
+++ t/request_tests/open_close_index.pl
@@ -9,6 +9,8 @@ my $r;
 
 ok $es->close_index( index => 'es_test_1' )->{ok}, 'Closed index';
 
+wait_for_es();
+
 throws_ok sub { $es->count( index => 'es_test_1', match_all => {} ) },
     qr/ElasticSearch::Error::ClusterBlocked/, ' - cluster blocked';
 
