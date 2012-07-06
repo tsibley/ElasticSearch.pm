5d63833e33c0f587372d9982395ad7100cb45f1a
Clinton Gormley <clinton@iannounce.co.uk>
2012-07-06 16:20:07 +0200

    Released version 0.55

--- lib/ElasticSearch.pm
+++ lib/ElasticSearch.pm
@@ -7,7 +7,7 @@ use ElasticSearch::Error();
 use ElasticSearch::RequestParser;
 use ElasticSearch::Util qw(throw parse_params);
 
-our $VERSION = '0.54';
+our $VERSION = '0.55';
 our $DEBUG   = 0;
 
 #===================================
@@ -155,7 +155,7 @@ ElasticSearch - An API for communicating with ElasticSearch
 
 =head1 VERSION
 
-Version 0.54, tested against ElasticSearch server version 0.19.7.
+Version 0.55, tested against ElasticSearch server version 0.19.8.
 
 =head1 DESCRIPTION
 
